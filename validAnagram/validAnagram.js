6076249677353c36a4213a32a7f222e17d98ca1a
Madelyn Romberg <madelynromberg@g.ucla.edu>
2023-07-14 19:03:41 -0700

    format doc

--- validAnagram/validAnagram.js
+++ validAnagram/validAnagram.js
@@ -6,23 +6,23 @@
  * Given two strings, return true if anagrams and false if not
  */
 
-function validAnagram(s,t){
+function validAnagram(s, t) {
   if (s.length !== t.length) return false;
   const sCounter = frequencyCounter(s);
   const tCounter = frequencyCounter(t);
 
-  for (const char in sCounter){
-      if (sCounter[char] != tCounter[char]){
-          return false;
-      };
-  }
+  for (const char in sCounter) {
+    if (sCounter[char] != tCounter[char]) {
+      return false;
+    };
+  };
 
   return true;
-
 };
 
-function frequencyCounter (str) {
+function frequencyCounter(str) {
   let counter = {};
   str.split("").forEach(s => (counter[s] ? counter[s]++ : counter[s] = 1));
+
   return counter;
 };
