4f3f19f215a99e408ad359dda57e17bb98af1513
Madelyn Romberg <madelynromberg@g.ucla.edu>
2023-07-14 19:02:58 -0700

    solve validAnagram

---
+++ validAnagram/validAnagram.js
@@ -0,0 +1,28 @@
+"use strict";
+
+/**
+ * validAnagram
+ *
+ * Given two strings, return true if anagrams and false if not
+ */
+
+function validAnagram(s,t){
+  if (s.length !== t.length) return false;
+  const sCounter = frequencyCounter(s);
+  const tCounter = frequencyCounter(t);
+
+  for (const char in sCounter){
+      if (sCounter[char] != tCounter[char]){
+          return false;
+      };
+  }
+
+  return true;
+
+};
+
+function frequencyCounter (str) {
+  let counter = {};
+  str.split("").forEach(s => (counter[s] ? counter[s]++ : counter[s] = 1));
+  return counter;
+};
